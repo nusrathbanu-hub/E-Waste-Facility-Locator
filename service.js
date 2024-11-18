0337752a250797ddec90ce44849749073a7a24bf
Nusrath Banu k <nusrathbanu826@gmail.com>
2024-11-18 20:13:12 +0530

    Add files via upload

---
+++ service.js
@@ -0,0 +1,63 @@
+
+let popup=document.getElementById("popup");
+let popup1=document.getElementById("popup1");
+let popup2=document.getElementById("popup2");
+let popup3=document.getElementById("popup3");
+let popup4=document.getElementById("popup4");
+let popup5=document.getElementById("popup5");
+
+
+            
+        function openpopup(){
+            popup.classList.add("open-popup");
+            }
+
+        function closepopup(){
+             popup.classList.remove("open-popup");
+            }
+
+
+        function print(){
+            popup1.classList.add("open-popup");
+            }
+
+        function display(){
+            popup1.classList.remove("open-popup");
+            }
+
+
+        function service(){
+            popup2.classList.add("open-popup");
+            }
+
+        function viewmore(){
+            popup2.classList.remove("open-popup");
+            }
+
+            function my_function(){
+                popup3.classList.add("open-popup");
+                }
+    
+            function my_function1(){
+                popup3.classList.remove("open-popup");
+                }
+
+                function ewaste(){
+                    popup4.classList.add("open-popup");
+                    }
+        
+                function recycling(){
+                    popup4.classList.remove("open-popup");
+                    }
+
+                    function disposal(){
+                        popup5.classList.add("open-popup");
+                        }
+            
+                    function Collection(){
+                        popup5.classList.remove("open-popup");
+                        }
+
+            
+
+            
